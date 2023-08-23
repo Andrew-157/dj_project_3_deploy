721d7344149a9c45df0c9b9d1e4dd1d9580bfc30
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-23 09:08:15 +0200

    Update settings.py

--- cookie/settings.py
+++ cookie/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['web-production-5bce.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-5bce.up.railway.app', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-5bce.up.railway.app/']
 
